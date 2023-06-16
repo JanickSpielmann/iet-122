5c2c9468ee816cb5dfcd271f873e492181d56208
JanickSpielmann <janick.spielmann@messageme.ch>
2023-06-16 17:19:09 +0200

    some changes

--- integer.py
+++ integer.py
@@ -18,3 +18,18 @@ print("a=5, b=6 c= "+ str(c))
 print("Division mit / und // Operator")
 print(str(100-36/10)+" / Operator")
 print(str(100-36//10)+" // Operator")
+
+c = True
+d = 10
+
+if (c): 
+    print("c is true")
+
+if (d is not 5):
+    print("d is not 5")
+
+for x in range(5,20):
+    print(str(x))
+
+if(d is 5 or 6 or 10):
+    print("true")
